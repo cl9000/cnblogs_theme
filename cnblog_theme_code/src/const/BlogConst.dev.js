34d97f0cee0620b1be9a0ea6dbc922f6ad04372c
weichanglu <cl9000@126.com>
2020-04-19 22:16:58 +0800

    first commit

--- cnblog_theme_code/src/const/BlogConst.dev.js
+++
@@ -1,5 +0,0 @@
-let isForward = true;//是否转发测试
-let baiduCount = "";
-let extendApiPath = "https://www.cjunn.xyz/cnblog-api";
-extendApiPath = "";
-export default {isForward, baiduCount, extendApiPath};
